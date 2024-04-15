176332a8dc0812859b1bddd9f484df95c5dd17c1
Ashfan Alam <97113157+Alam-313@users.noreply.github.com>
2024-04-15 18:33:09 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import { useState } from "react";
+import Navigation from "./components/Navigation";
+import HeroSection from "./components/Hero";
+const App=()=>{
+   
+   
+    return(
+        <>
+        
+        <div>
+            <Navigation/>
+            <HeroSection/>
+        </div>
+        </>
+    )
+}
+export default App;
